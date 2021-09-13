b12e77e73f8efcd9a71257eba5e0978aecad1659
Darshan <35395802+darshan1991@users.noreply.github.com>
2021-09-13 18:38:22 +0530

    replaced devConfig apiHostUrl

--- Frontend/src/utils/config/devConfig.ts
+++ Frontend/src/utils/config/devConfig.ts
@@ -1,5 +1,5 @@
 // tslint:disable: max-line-length
 export const devConfig = {
   apiHostUrl:
-    'https://su9f8lwsmc.execute-api.us-east-1.amazonaws.com/development',
+    'https://qe1lgcnkwh.execute-api.us-east-1.amazonaws.com/development',
 };
