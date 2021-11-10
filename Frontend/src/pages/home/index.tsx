5b1fc6219cf0a98d320fba94b78671e39fbe025c
Sasidharan <sasi2013dharan@gmail.com>
2021-11-10 09:55:27 +0530

    Added LatestNews feature

--- Frontend/src/pages/home/index.tsx
+++ Frontend/src/pages/home/index.tsx
@@ -1,12 +1,13 @@
 import React, { useEffect, useState } from 'react';
-import { useActions, setSystemDetails, setCurrentPage } from '../../actions';
-import { Http } from '../../utils';
-import { Container } from '@material-ui/core';
-import GigatesterBg from './gigatester_bg.jpeg';
+// import { useActions, setSystemDetails, setCurrentPage } from '../../actions';
+// import { Http } from '../../utils';
+import { Container, Grid, Typography } from '@material-ui/core';
+// import GigatesterBg from './gigatester_bg.jpeg';
 import { makeStyles } from '@material-ui/styles';
-import { useSelector } from 'react-redux';
-import { IRootState } from '../../reducers';
-
+// import { useSelector } from 'react-redux';
+// import { IRootState } from '../../reducers';
+import { LatestNews } from '../../components/home/rightPane';
+import  PageFooter  from '../../components/pageFooter';
 const useStyles = makeStyles({
   root: {
     width: '100%',
@@ -31,41 +32,41 @@ const useStyles = makeStyles({
 
 const Home = (props: any) => {
   const classes = useStyles();
-  const setSysDetails = useActions(setSystemDetails);
-  const userStatus = useSelector((state: IRootState) => state.user.idToken);
-  const systemDetails = useSelector((state: IRootState) => state.systemDetails);
-  const stateVariable = useSelector((state: IRootState) => state);
-  const setCurrentPageValue = useActions(setCurrentPage);
+  // const setSysDetails = useActions(setSystemDetails);
+  // const userStatus = useSelector((state: IRootState) => state.user.idToken);
+  // const systemDetails = useSelector((state: IRootState) => state.systemDetails);
+  // const stateVariable = useSelector((state: IRootState) => state);
+  // const setCurrentPageValue = useActions(setCurrentPage);
 
   // console.log(userStatus);
 
-  useEffect(() => {
-    setCurrentPageValue('');
-    if (
-      !systemDetails ||
-      systemDetails.appClientId === '' ||
-      systemDetails.appClientURL === ''
-    ) {
-      Http.get({
-        url: '/api/v2/settings/cognito',
-        state: { stateVariable },
-        customHeaders: { noauthvalidate: 'true' },
-      })
-        .then((response: any) => {
-          setSysDetails(response);
-        })
-        .catch((error: any) => {
-          props.history.push('/error');
-        });
-    }
-  }, []);
+  // useEffect(() => {
+  //   setCurrentPageValue('');
+  //   if (
+  //     !systemDetails ||
+  //     systemDetails.appClientId === '' ||
+  //     systemDetails.appClientURL === ''
+  //   ) {
+  //     Http.get({
+  //       url: '/api/v2/settings/cognito',
+  //       state: { stateVariable },
+  //       customHeaders: { noauthvalidate: 'true' },
+  //     })
+  //       .then((response: any) => {
+  //         setSysDetails(response);
+  //       })
+  //       .catch((error: any) => {
+  //         props.history.push('/error');
+  //       });
+  //   }
+  // }, []);
 
-  let redirectUrl: string;
-  if (userStatus) {
-    redirectUrl = '/auth';
-  } else {
-    redirectUrl = `https://${systemDetails.appClientURL}/login?response_type=token&client_id=${systemDetails.appClientId}&redirect_uri=https://${window.location.host}/auth`;
-  }
+  // let redirectUrl: string;
+  // if (userStatus) {
+  //   redirectUrl = '/auth';
+  // } else {
+  //   redirectUrl = `https://${systemDetails.appClientURL}/login?response_type=token&client_id=${systemDetails.appClientId}&redirect_uri=https://${window.location.host}/auth`;
+  // }
 
   return (
     // tslint:disable-next-line: jsx-wrap-multiline
@@ -75,12 +76,34 @@ const Home = (props: any) => {
         root: classes.root,
       }}
     >
-      <img
+       <Grid container spacing={1} >
+        {/* <TopPane /> */}
+        <Grid item xs={12} sm={1} />
+        <Grid item xs={12} sm={6}>
+          <Grid item xs={12} sm={12} style={{ marginTop: '10px' }}>
+            {/* <RocordsCount /> */}
+          </Grid>
+          <Grid item xs={12} sm={12} style={{ marginTop: '15px' }}>
+            {/* <Typography style={{ fontWeight: 'bold', fontStyle: 'italic' }}>BUZZ</Typography>
+            <UsersFeedback /> */}
+          </Grid>
+          </Grid>
+          <Grid item xs={12} sm={4} style={{ marginTop: '10px' }}>
+          <Typography style={{ fontWeight: 'bold', fontStyle: 'italic' }}>Latest News </Typography>
+          <LatestNews />
+          <LatestNews />
+          <LatestNews />
+          </Grid>
+          <PageFooter />
+          </Grid>
+          
+
+      {/* <img
         className={classes.img}
         src={GigatesterBg}
         alt='Are you doing it right?'
       />
-      <div className={classes.textAlign}>GigaTester is a product to test applications by <br /> <span style={{ paddingLeft: '80px' }}> External/Freelance testers </span> </div>
+      <div className={classes.textAlign}>GigaTester is a product to test applications by <br /> <span style={{ paddingLeft: '80px' }}> External/Freelance testers </span> </div> */}
     </Container>
   );
 };
