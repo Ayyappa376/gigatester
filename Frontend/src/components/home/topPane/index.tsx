83bd67bb808aa5729ec6fc19103caf8a3e536ddf
Darshan <35395802+darshan1991@users.noreply.github.com>
2021-11-16 11:37:24 +0530

    changes in alignment

--- Frontend/src/components/home/topPane/index.tsx
+++ Frontend/src/components/home/topPane/index.tsx
@@ -3,10 +3,10 @@ import { makeStyles } from '@material-ui/core/styles';
 import { Button, Grid, InputLabel, Paper, Typography } from '@material-ui/core';
 import '../style.css';
 
-const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles(() => ({
     root: {
         background: '#F0F0F0',
-        // padding: '10px',
+        paddingTop: '10px',
         fontFamily: 'Montserrat',
         width: '100%'
     },
@@ -26,8 +26,8 @@ const TesterEvents = () => {
     return (
         <Paper className={classes.root} data-testid="testerEvents">
             <Grid container spacing={1} >
-                <Grid item xs={12} sm={1} />
-                <Grid item xs={12} sm={4} style={{ textAlign: 'right' }}>
+                <Grid item xs={1} sm={1} />
+                <Grid item xs={11} sm={4} style={{ textAlign: 'right' }}>
                     <Typography className={classes.title} >Become a Tester?</Typography>
                     <InputLabel className={classes.subTitle}>Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has been the industry standard dummy text</InputLabel>
                     <Button variant="outlined" color="primary" size='small' className='button buttonMarginTopPane' data-testid="signUp">
@@ -35,14 +35,14 @@ const TesterEvents = () => {
                     </Button>
                 </Grid>
                 <Grid item xs={12} sm={2} />
-                <Grid item xs={12} sm={4} style={{ textAlign: 'left' }}>
+                <Grid item xs={11} sm={4} style={{ textAlign: 'left' }}>
                     <Typography className={classes.title}>Test my Product?</Typography>
                     <InputLabel className={classes.subTitle}>Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has been the industry standard dummy text</InputLabel>
                     <Button variant="outlined" color="primary" size='small' className='button buttonMarginTopPane' data-testid="signIn">
                         Click Here
                     </Button>
                 </Grid>
-                <Grid item xs={12} sm={1} />
+                <Grid item xs={1} sm={1} />
             </Grid>
         </Paper >
     );
