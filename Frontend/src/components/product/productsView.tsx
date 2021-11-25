47ad469e4c3db6762ad005288340f8185fc3104f
Gargi Basak <gargi.basak@pinimbus.com>
2021-11-25 18:48:49 +0530

    fix for showing only products of active campaigns to testers

--- Frontend/src/components/product/productsView.tsx
+++ Frontend/src/components/product/productsView.tsx
@@ -41,13 +41,13 @@ const ProductsView = (props: any) => {
     const [allDevices, setAllDevices] = useState<IDeviceInfo[]>([]);
     const [selectedDevice, setSelectedDevice] = useState<any>('');
     const [selectedPlatform, setSelectedPlatform] = useState<any>('');
+    const [productsFetched, setProductsFetched] = useState<boolean>(false);
 
     const history = useHistory();
 
     useEffect(() => {
         fetchAllPlatforms();
         fetchAllDevices();
-        //    fetchAllTestSuites();
         fetchCampaignDetails();
     }, []);
 
@@ -98,7 +98,7 @@ const ProductsView = (props: any) => {
     const fetchCampaignDetails = () => {
         let products: any[] = [];
         Http.get({
-            url: `/api/v2/campaigns?status${STATUS_CAMPAIGN_ACTIVE}`,
+            url: `/api/v2/campaigns?status=${STATUS_CAMPAIGN_ACTIVE}`,
             state: stateVariable,
         })
             .then((response: any) => {
@@ -107,9 +107,11 @@ const ProductsView = (props: any) => {
                         products.push(item)
                     })
                 })
-                setAllProducts(products)
+                setAllProducts(products);
+                setProductsFetched(true);
             })
             .catch((error: any) => {
+                setProductsFetched(true);
                 const perror = JSON.stringify(error);
                 const object = JSON.parse(perror);
                 if (object.code === 401) {
@@ -186,7 +188,7 @@ const ProductsView = (props: any) => {
                     />
                 </Grid>
             </Grid>
-            {allProducts.length ?
+            {productsFetched ?
                 <Grid container spacing={2} >
                     <Grid item xs={12} sm={6}>
                         <Typography data-testid="header" >Showing {allProducts.length} products</Typography>
@@ -210,7 +212,8 @@ const ProductsView = (props: any) => {
                             </Grid>
                         )
                     })} */}
-                    {allProducts.length && allProducts.map((item: any, index: number) => {
+                    {allProducts.length ?
+                      (allProducts.map((item: any, index: number) => {
                         let platforms = allPlatforms.filter(p1 => item.platforms.some((p2: any) => p1.id === p2));
                         let devices = allDevices.filter(d1 => item.devices.some((d2: any) => d1.id === d2));
 
@@ -242,7 +245,10 @@ const ProductsView = (props: any) => {
                                 </Paper>
                             </Grid>
                         )
-                    })}
+                      })
+                      ) : (
+                          <div/>
+                      )}
                 </Grid>
                 : (
                     <Container className='loaderStyle'>
