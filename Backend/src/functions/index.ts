77f7a7a7a3c64b42ca45ac9ceda96011717f6d0b
sasidharan-pinimbus <95898250+sasidharan-pinimbus@users.noreply.github.com>
2021-12-17 11:30:45 +0530

    added feedback function

--- Backend/src/functions/index.ts
+++ Backend/src/functions/index.ts
@@ -6,38 +6,35 @@ cors({
     origin: true,
   });
 exports.handler = async (event: any) => {
-    console.log('Received event:', JSON.stringify(event, null, 2));
+    // console.log('Received event:', JSON.stringify(event, null, 2));
     let body;
     let statusCode = '200';
     const headers = {
         'Access-Control-Allow-Origin':'*',
         'Content-Type': 'application/json'
     };
-   
     try {
         switch (event.httpMethod) {
             case 'DELETE':
                 body = await dynamo.delete(JSON.parse(event.body)).promise();
                 break;
             case 'GET':
-                body = await dynamo.scan({ TableName: "dev_GT_feedback" }).promise();
+                body = await dynamo.scan({ TableName: 'dev_GT_feedback' }).promise();
                 break;
             case 'POST':
                 const jsonBody = JSON.parse(event.body);
-                console.log(jsonBody.body.data.id)
                 const tableparams = {
-                            TableName: "dev_GT_feedback",
                             Item: {
-                                "id": jsonBody.id,
-                                "userId": jsonBody.userId,
-                                "createdOn": Date.now(),
-                                "productRating": jsonBody.productRating,
-                                "productVersion": jsonBody.productVersion,
-                                "feedbackComments": jsonBody.feedbackComments,
-                                "productId": jsonBody.productId,
-                                "productName": jsonBody.productName
-                            }
-                        }
+                                createdOn: Date.now(),
+                                feedbackComments: jsonBody.feedbackComments,
+                                id: jsonBody.id,
+                                productId: jsonBody.productId,
+                                productRating: jsonBody.productRating,
+                                productVersion: jsonBody.productVersion,
+                                userId: jsonBody.userId,
+                            },
+                            TableName: 'dev_GT_feedback'
+                        };
                 body = await dynamo.put(tableparams).promise();
                 break;
             case 'PUT':
@@ -45,28 +42,24 @@ exports.handler = async (event: any) => {
                 break;
             case 'OPTIONS':
                 statusCode = '204';
-                
-                headers["Access-Control-Allow-Headers"] = "*";
-                headers["Access-Control-Allow-Origin"] = "*";
-                headers["Access-Control-Allow-Methods"] = "HEAD, GET, POST, PUT, DELETE";
-                body={};
-                
+                headers['Access-Control-Allow-Headers'] = '*';
+                headers['Access-Control-Allow-Origin'] = '*';
+                headers['Access-Control-Allow-Methods'] = 'HEAD, GET, POST, PUT, DELETE';
+                body= {};
                 break;
             default:
                 throw new Error(`Unsupported method "${event.httpMethod}"`);
         }
-    }catch (err) {
+    } catch (err) {
         statusCode = '400';
         console.error('GT_AppFeedback Failed.', err);
         // body = err.message;
     } finally {
         body = JSON.stringify(body);
     }
-
     return {
-        statusCode,
         body,
         headers,
+        statusCode,
     };
 };
-
