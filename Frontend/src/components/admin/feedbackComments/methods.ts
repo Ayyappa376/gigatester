698d0bf5b7beac7e249cbb1ea7de4aac7e17bf99
Rachit kohli <Rachit.kohli@pinimbus.com>
2022-01-07 08:54:52 +0530

    [Rachit]: Fixes made for the charts

--- Frontend/src/components/admin/feedbackComments/methods.ts
+++ Frontend/src/components/admin/feedbackComments/methods.ts
@@ -4,7 +4,7 @@ export const processBarChartData = (items: IAppFeedback[]) => {
     let ratingData : IProcessedData = {"1" : 0, "2" : 0, "3" : 0, "4" : 0, "5" : 0};
     if(items.length > 0) {
         items.forEach((item) => {
-            if(item.productRating) {
+            if(item.productRating && typeof item.productRating !== 'string') {
                 ratingData[item.productRating.toString()]++;
             }  
         })
