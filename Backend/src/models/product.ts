fc6b6ed9e53728e1d0e2cf430d589bf6bcba65eb
Gargi Basak <gargi.basak@pinimbus.com>
2022-01-16 23:40:34 +0530

    corrected the object model for product.

--- Backend/src/models/product.ts
+++ Backend/src/models/product.ts
@@ -1,17 +1,27 @@
 export interface ProductInfo {
+    apiId?:string;
+    apiKey?: string;
     description?: string;
     devices?: string[];
+    categories?: Category[];
     id: string;
     name: string;
     platforms: string[];
     software?: string;
+    softwareType?:string;
     testers?: TesterStatus[];
-    testSuite?: string;
+    testSuite?: string[];
     version: string[];
     [keyName: string]: any;
 }
 
+
 export interface TesterStatus {
     approved: boolean;
     id: string;
+}
+
+export interface Category {
+    name?: string;
+    feedbacks?: string[];
 }
--- Frontend/src/model/admin/product.ts
+++ Frontend/src/model/admin/product.ts
@@ -1,24 +1,32 @@
 import  { IObjectConfigDetails } from '..';
 export interface IProductInfo {
-    apiKey?:string;
-    apiId?:string;
-    id: string;
-    name: string;
-    description?: string;
-    devices?: string[];
-    software?: string;
-    softwareType?:string;
-    platforms: string[];
-    testSuite?: string[];
-    testers?: ITesterStatus[];
-  }
-  
-  export interface ITesterStatus {
-      id: string;
-      approved: boolean;
-  }
+  apiId?:string;
+  apiKey?:string;
+  id: string;
+  version: string[];
+  name: string;
+  description?: string;
+  devices?: string[];
+  software?: string;
+  softwareType?:string;
+  platforms: string[];
+  testSuite?: string[];
+  testers?: ITesterStatus[];
+  categories?: Category[];
+  [keyName: string]: any;
+}
 
-  export interface IProductParams {
-    productConfig: IObjectConfigDetails;
-    products?: IProductInfo[];
+export interface ITesterStatus {
+  id: string;
+  approved: boolean;
+}
+
+export interface Category {
+  name?: string;
+  feedbacks?: string[];
+}
+
+export interface IProductParams {
+  productConfig: IObjectConfigDetails;
+  products?: IProductInfo[];
 }
