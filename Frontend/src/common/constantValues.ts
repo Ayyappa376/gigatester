69c0272ad291885e95b9a6cd60c0ffec9f919aae
Gargi Basak <gargi.basak@pinimbus.com>
2022-03-03 19:29:42 +0530

    removed unused files

--- Frontend/src/common/constantValues.ts
+++
@@ -1,6 +0,0 @@
-export const TRIAL_MODE = 'trial';
-export const NORMAL_MODE = 'normal';
-export const TRIAL_USERNAME = 'gargibasak@yahoo.com';
-export const TRIAL_PASSWORD = 's0meTh(ng)';
-export const TRIAL_QUESTIONNAIRE_ID = '2323';
-export const TRIAL_QUESTIONNAIRE_VERSION = '1';
