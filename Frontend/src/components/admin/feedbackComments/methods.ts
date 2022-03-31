2e1f00e188dfaabbafa2cdb537f6160750257e1b
Gargi Basak <gargi.basak@pinimbus.com>
2022-03-31 07:54:37 +0000

    Squashed commit of the following
    commit c824cb13 
    Author: Francisco <fveranicola@gmail.com> 
    Date: Thu Mar 31 2022 05:49:40 GMT+0530 (India Standard Time) 
    
        commented out free rotation customization
    
    
    commit 7415b3d0 
    Author: Francisco <fveranicola@gmail.com> 
    Date: Thu Mar 31 2022 05:47:49 GMT+0530 (India Standard Time) 
    
        Refactore/re-arranged collapse row
    
    
    commit ddb3d3ec 
    Author: Francisco <fveranicola@gmail.com> 
    Date: Thu Mar 31 2022 03:25:50 GMT+0530 (India Standard Time) 
    
        removed widget free rotation, working on expandable rows
    
    
    commit 580edd75 
    Author: Francisco <fveranicola@gmail.com> 
    Date: Thu Mar 31 2022 00:25:21 GMT+0530 (India Standard Time) 
    
        added measure for product and version filter to only load data when both categories are selected

--- Frontend/src/components/admin/feedbackComments/methods.ts
+++ Frontend/src/components/admin/feedbackComments/methods.ts
@@ -1,6 +1,7 @@
 import { IRootState } from "../../../reducers";
 import { Http } from "../../../utils";
-import { CONST_BUG_REPORT, CONST_FEEDBACK, CONST_BUG_REPORT_CHART, CONST_FEEDBACK_CHART } from "./common";
+import { CONST_BUG_REPORT, CONST_FEEDBACK, CONST_BUG_REPORT_CHART, CONST_FEEDBACK_CHART, IFetchRecursiveData, NUMBER_OF_ITEMS_PER_FETCH } from "./common";
+import { RouteComponentProps, withRouter } from 'react-router-dom';
 
 export const getSignedUrl = async(url: string, stateVariable: IRootState) => {
     return new Promise((resolve, reject) => {
@@ -89,4 +90,62 @@ export const getFeedbckChartData = async({ setFeedbackBarChartData, setBugBarCha
     .catch((error) => {
       console.error(error)
     });
+  }
+
+
+interface productDetails {
+  productInfo: any,
+  prodNameIdMapping: any,
+  prodNameIdMappingBugCopy: any,
+  setProductInfo: Function,
+  setProdNameIdMapping: Function,
+  setSelectedProdId: Function,
+  setProdNameIdMappingBugs: Function,
+  setProductVersion: Function,
+  productId: any,
+  productVersion: any,
+  }
+
+export const getProductDetails = async ({ productInfo, prodNameIdMapping, prodNameIdMappingBugCopy, setProductInfo, setProdNameIdMapping, setSelectedProdId, setProdNameIdMappingBugs, setProductVersion, productId, productVersion  }: productDetails) => {
+  Http.get({
+    url: `/api/v2/products`,
+  }).then((response: any) => {
+    if (response && response.products && Array.isArray(response.products) && response.products.length > 0) {
+      const productInfoCopy = [...productInfo]
+      const prodNameIdMappingCopy: any = { ...prodNameIdMapping };
+      response.products.forEach((el: any) => {
+        const prodInfo = { id: "", name: "" };
+        prodInfo.id = el.id;
+        prodInfo.name = el.name;
+        productInfoCopy.push(prodInfo);
+        if (prodNameIdMappingCopy[prodInfo.id]) {
+          prodNameIdMappingCopy[prodInfo.id].version.push(el.version);
+        } else {
+          prodNameIdMappingBugCopy[prodInfo.id] = {
+            name: prodInfo.name,
+            version: [el.version][0],
+            categories: el.feedbackAgentSettings ? el.feedbackAgentSettings.bugSettings.categories ? el.feedbackAgentSettings.bugSettings.categories : [] : []
+          }
+          prodNameIdMappingCopy[prodInfo.id] = {
+            name: prodInfo.name,
+            version: [el.version],
+            categories: el.feedbackAgentSettings ? el.feedbackAgentSettings.feedbackSettings.categories ? el.feedbackAgentSettings.feedbackSettings.categories : [] : []
+          }
+        }
+      })
+      setProductInfo(productInfoCopy);
+      setProdNameIdMapping(prodNameIdMappingCopy);
+      setProdNameIdMappingBugs(prodNameIdMappingBugCopy);
+      if (productId && productVersion) {
+        return;
+      }
+      const defaultProductId = Object.keys(prodNameIdMappingCopy)[0];
+      setSelectedProdId(defaultProductId);
+      setProductVersion(prodNameIdMappingCopy[defaultProductId].version[0]);
+    }
+  }).catch((error: any) => {
+    console.error(error);
+  })
 }
+
+
