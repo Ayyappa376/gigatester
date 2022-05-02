89443ee7d4933123741d803aaf191c40d0960192
Francisco <fveranicola@gmail.com>
2022-05-02 11:28:05 -0400

    refactoring/minifying some code

--- Frontend/src/components/admin/products/email/ratingLimit.tsx
+++ Frontend/src/components/admin/products/email/ratingLimit.tsx
@@ -49,19 +49,14 @@ const RatingLimitButtons = ({
     6: false,
   });
   const [oldState, setOldState] = useState <stateProps>({});
-  const [ratingMap, setRatingMap] = useState<number[]>([])
   const [checkAll, setCheckAll] = useState<boolean>(false);
 	const classes = useStyles();
-  const RATE_LIMIT = 7; //remove 0th index, and add 7th which is all
 
   useEffect(() => {
     if (Object.keys(ratingObj).length > 0) {
       setState(ratingObj)
       setOldState(ratingObj)
     }
-    const rateMapping = Array.from(Array(RATE_LIMIT).keys());
-    rateMapping.shift();
-    setRatingMap(rateMapping);
   }, [ratingObj]);
 
   useEffect(() => {
@@ -73,6 +68,21 @@ const RatingLimitButtons = ({
       }
     }
   }, [checkAll])
+
+  useEffect(() => {
+    if (state[6]) {
+      for (let key in state) {
+        if (!state[key]) {
+          setState({
+            ...state,
+            6: false,
+          })
+          handleEmailRating({ key: 6, check: false })
+          return;
+        }
+      }
+    }
+  }, [state])
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     if (event.target.value === '6' && event.target.checked) {
@@ -101,12 +111,12 @@ const RatingLimitButtons = ({
 				Choose rating limit that activates email message - can select multiple *
 			</FormLabel>
 			<FormGroup className={classes.rowGroup}>
-				{ratingMap.length > 0 ? ratingMap.map((val: number, index) => (
+				{Object.keys(state).length > 0 ? Object.keys(state).map((val: any, index: number) => (
 					<FormControlLabel
 						key={index}
 						value={val}
             control={<Checkbox checked={state[val]} color='primary' onChange={handleChange}/>}
-						label={val !== 6 ? `${val} - ${ratingSymbol.toLowerCase()}` : 'All'}
+						label={val !== '6' ? `${val} - ${ratingSymbol.toLowerCase()}` : 'All'}
 					/>
 				)) : null}
 			</FormGroup>
