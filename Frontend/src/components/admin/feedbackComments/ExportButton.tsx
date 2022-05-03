222fb3c8b6290d8c0d6e04d8411ad20ca5d518ff
Francisco <fveranicola@gmail.com>
2022-05-03 11:07:43 -0400

    Filtered data is exported, added some extra formatting for excel file, more optimizations

--- Frontend/src/components/admin/feedbackComments/ExportButton.tsx
+++ Frontend/src/components/admin/feedbackComments/ExportButton.tsx
@@ -7,11 +7,15 @@ import * as XLSX from 'xlsx';
 const useStyles = makeStyles((theme) => ({
 	export: {
 		float: 'right',
-		height: '40px',
-		width: '150px',
+		height: '38px',
+		width: 'auto',
 		display: 'flex',
 		flexDirection: 'row',
 		...buttonStyle,
+		fontSize: '13px',
+	},
+	tooltip: {
+		fontSize: '12px',
 	},
 }));
 
@@ -21,10 +25,12 @@ interface exportProps {
 	type?: string;
 }
 
-const ExportBtn = ({ data, client }: exportProps) => {
+const ExportBtn = ({ data, client, type }: exportProps) => {
 	const [exportedData, setExportedData] = useState<any>([]);
 	const [enable, setDisable] = useState<boolean>(false);
 	const classes = useStyles();
+
+	// console.log('data', data);
 
 	const organizeData = (data: any) => {
 		const tempArray: any = [];
@@ -35,38 +41,55 @@ const ExportBtn = ({ data, client }: exportProps) => {
 				if (typeof current === 'object' && current !== null) {
 					if (Object.keys(current).length > 0) {
 						for (let subKey in current) {
+							let capSubKey = subKey.toUpperCase();
 							if (current[subKey]) {
-								temp[subKey] = current[subKey]
+								temp[capSubKey] = current[subKey]
 							} else {
-								temp[subKey] = 'No Data';
+								temp[capSubKey] = 'No Data';
 							}
 						}
 					} else if (Object.keys(current).length === 0) {
-						temp[key] = 'No Data';
+						temp[key.toUpperCase()] = 'No Data';
 					}
 				} else if (key === 'feedbackComments') {
 					let coms = JSON.parse(current);
 					for (let subComment in coms) {
+						let capSubComment = subComment.toUpperCase();
 						if (Array.isArray(coms[subComment])) {
 							coms[subComment].forEach((comment: string) => {
-								if (comment) {
-									temp[subComment] = comment;
+								if (comment.length > 0) {
+									temp[capSubComment] = comment;
 								} else {
-									temp[subComment] = 'No standard comments';
+									temp[capSubComment] = 'NO STANDARD COMMENTS';
 								}
 							})
 						} else {
-							temp[subComment] = coms[subComment];
+							if (coms[subComment]) {
+								temp[capSubComment] = coms[subComment];
+							} else {
+								temp[capSubComment] = 'NO GENERAL COMMENT';
+							}
+						}
+					}
+				} else if (key === 'platformInfo') {
+					let platInfo = JSON.parse(current);
+					for (let subInfo in platInfo) {
+						let capSubInfo = subInfo.toUpperCase();
+						if (subInfo) {
+							temp[capSubInfo] = platInfo[subInfo]
+						} else {
+							temp[capSubInfo] = 'No platform data'
 						}
 					}
 				} else {
+					let capKey = key.toUpperCase();
 					if (typeof current === 'number' && key === 'createdOn') {
 						const dateStamp = new Date(current);
-						temp[key] = dateStamp.toString();
+						temp[capKey] = dateStamp.toString();
 					} else if (!current) {
-						temp[key] = 'No Data';
+						temp[capKey] = 'No Data';
 					} else {
-						temp[key] = current;
+						temp[capKey] = current;
 					}
 				}
 			}
@@ -89,23 +112,28 @@ const ExportBtn = ({ data, client }: exportProps) => {
 	const handleExport = (event: any) => {
 		console.log('EXPORTING');
 		const day = new Date().toISOString().split('T')[0];
-		const title = client ? `${client}_${day}` : `'Product_${day}'`;
+		const title = client ? `${client}_${day}.xlsx` : `'Product_${day}.xlsx'`;
+		const dataType = type ? type : 'Feedback';
 
 		const workBook = XLSX.utils.book_new();
 		const workSheet = XLSX.utils.json_to_sheet(exportedData);
-		XLSX.utils.book_append_sheet(workBook, workSheet, 'FeedbackSheet');
+		XLSX.utils.book_append_sheet(workBook, workSheet, `${dataType}-Sheet`);
 		XLSX.writeFile(workBook, title);
   };
 
 	return (
-		<Tooltip title='Click to export selected product data into CSV/XLSX file' arrow>
+		<Tooltip title='Click to export selected or filtered product data into CSV/XLSX file'
+		classes={{
+			tooltip: classes.tooltip,
+		}}
+			arrow>
 			<Button
 				variant='contained'
 				className={classes.export}
 				onClick={(event) => handleExport(event)}
 				disabled={enable}
 			>
-				Export
+				Export {type}
 				<GetApp style={{ fontSize: '20px' }} />
 			</Button>
 		</Tooltip>
